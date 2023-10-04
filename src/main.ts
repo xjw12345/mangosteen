64d94c3c53871beeb3eca2b4d04f26ff66f52f7a
junweixiao <2849221297@qq.com>
2023-10-04 15:18:38 +0800

    refactor:重构main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,12 @@
 import { createApp } from 'vue'
 import {App} from './App'
-import { Bar } from './views/Bar'
-import { Foo } from './views/Foo'
-import {createRouter,createWebHashHistory} from 'vue-router'
+import {createRouter} from 'vue-router'
+import { routes } from './config/routes'
+import { history } from './shared/history'
 
 
-const routes = [
-  { path: '/', component: Foo },
-  { path: '/about', component: Bar },
-]
 
-const router = createRouter({
-  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: createWebHashHistory(),
-  routes, // `routes: routes` 的缩写
-})
+const router = createRouter({ history,routes,})
 
 const app =createApp(App)
 app.use(router)
